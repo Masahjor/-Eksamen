6dc1babfdaa71a5a2a5f30749499edd07a56593c
Masahjor <98082444+Masahjor@users.noreply.github.com>
2025-10-27 14:12:50 +0100

    moar edits

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -18,8 +18,10 @@
 <main>
     <header class="bg-black text-white min-h-[50px] relative overflow-hidden flex justify-center items-center">
 
+        
+
     <!-- Navigation -->
-    <nav class="flex m-auto" class:open={isMenuOpen}>
+    <nav class="m-auto lg:block sm:hidden " class:open={isMenuOpen}>
         <ul class="flex items-center">
             {#each sections as section}
             <li>
@@ -32,7 +34,7 @@
     <!-- Hamburger Menu Icon -->
     <!-- svelte-ignore a11y_click_events_have_key_events -->
     <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <div class="hidden sm:block bg-pink-500 absolute right-4 top-4 cursor-pointer z-10" on:click={toggleMenu}>
+    <div class="lg:hidden sm:block  absolute right-4 top-4 cursor-pointer z-10" on:click={toggleMenu}>
         <div class="w-8 h-1 bg-white mb-2 "></div>
         <div class="w-8 h-1 bg-white mb-2"></div>
         <div class="w-8 h-1 bg-white mb-2"></div>
